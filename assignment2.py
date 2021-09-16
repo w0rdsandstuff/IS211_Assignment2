55bd082a677815c7607a0e75b291b26ba4d6f19f
w0rdsandstuff <77218212+w0rdsandstuff@users.noreply.github.com>
2021-09-16 10:26:23 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -5,76 +5,42 @@ import datetime
 
 def downloadData(url):
     """Downloads the data"""
-    request = urllib2.Request(url)
-    urldata = urllib2.urlopen(request)
-    return urldata.read()
+    response = urllib.request.urlopen(url)
+    data = response.read().decode('utf-8')
+    return data
 
-def processData(file_content):
-    csv_reader = csv.reader(csvdata)
-    personData = {}
-    line_count = 0
-    date_format = '%d/%m/%Y'
-    csv_data = []
-    for csv_data in csv_reader:
-        id = csv_data[0]
-        username = csv_data[1]
-        birthday = csv_data[2]
-        if id != 'id':
-            id = int(id)
-            # csv_data[0] = int(csv_data[0])
-            line_count += 1
-            try:
-                birthday = datetime.datetime.strftime(csv_data[2], date_format)
-            except Exception as e:
-                logging.error('Error processing line # %s for id # %s' % (line_count, id))
-            finally:
-                personData[id] = {'name': username, 'birthday': birthday}
-            return personData
-
-
-def displayPerson(id, personData):
-    try:
-        print 'Person # %s is %s with a birthday of %s' % (id, personData[id][name], personData[id][dob])
-    except Exception, e:
-        logging.warning('No user found with that id')
-
-def main(url):
-    """
-
-    :return:
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--url', help='Type the URL of csv file', action="store", dest="url", type=str)
-    args = parser.parse_args()
-
-    csvdata1 = processData(args.url)
-
-    if args.url:
-        csvdata1 = downloadData(url)
-        personData = processData(csvdata1)
-
-    while True:
-        try:
-            user_input = int(raw_input('Please enter and ID # . Enter 0 or a negative $ to exit:'))
-        except ValueError:
-            logging.info('User typed wrong id')
-            print 'Invalid Input. Please try agaiin'
+def processData(file_contents):
+        empty_dict = {}
+    header = True
+    for line in file_contents.splitlines():
+        if header:
+            header = False
             continue
-        if user_input > 0:
-            displayPerson(user_input, personData)
-        else:
-            print 'Thank you'
-            sys.exit()
+        parts = line.split(',')
+        try:
+            user_id = int(parts[0])
+            name = parts[1]
+            birthday = datetime.datetime.strptime(parts[2],'%d/%m/%Y') 
+            empty_dict[int(parts[0])] = (name, birthday)
+        except ValueError as e:
+            print(e)
+            print(f'Error on line {line}')
+        
 
-    print(f"Running main with URL = {url}...")
-    
-if __name__ == "__main__":
-    main()
+    return empty_dict
+
+def displayPerson(user_id, empty_dict):
+    name, birthday = empty_dict[user_id]
+    print(f'{user_id},{name} born on {birthday}')
 
 
 if __name__ == "__main__":
     """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
+    url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
+    data = downloadData(url)
+    results = processData(data)
+    while True:
+        userid = int(input('Please enter an ID number:  ').strip())
+        if userid <= 0:
+            break
+        displayPerson(userid, results
